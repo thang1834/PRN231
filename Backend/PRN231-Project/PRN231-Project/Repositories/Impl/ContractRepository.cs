d7ae1998e25d3afc355cc8d8148111c40fc713e2
viethq <quocviet13122002@gmail.com>
2024-03-03 00:47:03 +0700

    init contract repository and service

---
+++ Backend/PRN231-Project/PRN231-Project/Repositories/Impl/ContractRepository.cs
@@ -0,0 +1,47 @@
+﻿using PRN231_Project.Models;
+
+namespace PRN231_Project.Repositories.Impl
+{
+    public class ContractRepository : IContractRepository
+    {
+        public Task AddContractAsync(Contract contract)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contract>> GetAllContractsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contract> GetContractByIdAsync(int contractId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contract>> GetContractsByHouseIdAsync(int houseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contract>> GetContractsByUserIdAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contract>> GetContractsInDateRangeAsync(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveContractAsync(int contractId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateContractAsync(Contract contract)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
