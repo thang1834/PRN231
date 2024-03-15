97c44e4506f5147782cae91d9ee78bf7269400d0
thang1834 <90250933+thang1834@users.noreply.github.com>
2024-03-15 09:03:45 +0700

    create transaction

---
+++ Backend/PRN231-Project/PRN231-Project/Dto/Transaction/TransactionDto.cs
@@ -0,0 +1,20 @@
+﻿namespace PRN231_Project.Dto.Transaction
+{
+    public class TransactionDto
+    {
+        public int Id { get; set; }
+        public string? Tid { get; set; }
+        public string? Description { get; set; }
+        public decimal? Amount { get; set; }
+        public decimal? CusumBalance { get; set; }
+        public string? When { get; set; }
+        public string? BankSubAccId { get; set; }
+        public string? SubAccId { get; set; }
+        public string? VirtualAccount { get; set; }
+        public string? VirtualAccountName { get; set; }
+        public string? CorresponsiveName { get; set; }
+        public string? CorresponsiveAccount { get; set; }
+        public string? CorresponsiveBankId { get; set; }
+        public string? CorresponsiveBankName { get; set; }
+    }
+}
---
+++ Backend/PRN231-Project/PRN231-Project/Dto/Transaction/TransactionResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PRN231_Project.Dto.Transaction
+{
+    public class TransactionResponse
+    {
+        public int Error { get; set; }
+        public List<TransactionDto> Data { get; set; }
+        public TransactionResponse()
+        {
+            Error = 0;
+            Data = new List<TransactionDto>();
+        }
+    }
+}
