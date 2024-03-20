882eed7293d426d9510f143ba11ec0333bf3e2b3
thanh289211 <122135348+thanh289211@users.noreply.github.com>
2024-03-20 10:00:08 +0700

    fix button error

--- FrontEnd/src/views/welcome/Welcome.js
+++ FrontEnd/src/views/welcome/Welcome.js
@@ -24,7 +24,7 @@ const Welcome = () => {
                     management team.
                 </p>
                 <div class="hero-actions">
-                    <button class="btn hover-effect" onclick="">
+                    <button class="showcase hover-effect" onclick="">
                         Showcase
                     </button>
                 </div>
