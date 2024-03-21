79952b9c1c2ad7f0393f79261a1c1e2a2ec73fbf
ducanh0310 <nguyenducanhflm@gmail.com>
2024-03-21 21:18:21 +0700

    Update Statistic.js

--- FrontEnd/src/views/statistic/Statistic.js
+++ FrontEnd/src/views/statistic/Statistic.js
@@ -157,13 +157,7 @@ const Statistic = () => {
                             <CButton color="primary" className="float-end">
                                 <CIcon icon={cilCloudDownload} />
                             </CButton>
-                            <CButtonGroup className="float-end me-3">
-                                {['Day', 'Month', 'Year'].map((value) => (
-                                    <CButton color="outline-secondary" key={value} className="mx-0" active={value === 'Month'}>
-                                        {value}
-                                    </CButton>
-                                ))}
-                            </CButtonGroup>
+
                         </CCol>
                     </CRow>
                     <CChartLine
@@ -230,19 +224,13 @@ const Statistic = () => {
                             <h4 id="traffic" className="card-title mb-0">
                                 Payment
                             </h4>
-                            <div className="small text-medium-emphasis">January - July 2021</div>
+                            <div className="small text-medium-emphasis">January - December</div>
                         </CCol>
                         <CCol sm={7} className="d-none d-md-block">
                             <CButton color="primary" className="float-end">
                                 <CIcon icon={cilCloudDownload} />
                             </CButton>
-                            <CButtonGroup className="float-end me-3">
-                                {['Day', 'Month', 'Year'].map((value) => (
-                                    <CButton color="outline-secondary" key={value} className="mx-0" active={value === 'Month'}>
-                                        {value}
-                                    </CButton>
-                                ))}
-                            </CButtonGroup>
+
                         </CCol>
                     </CRow>
                     <CChartLine
