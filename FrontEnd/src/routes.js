9e6844ad05efb641aa53f8b18230b0691a5650af
ducanh0310 <nguyenducanhflm@gmail.com>
2024-03-21 19:37:25 +0700

    statistic

--- FrontEnd/src/routes.js
+++ FrontEnd/src/routes.js
@@ -12,6 +12,9 @@ const Contract = React.lazy(() => import('./views/contract/Contract'));
 
 const House = React.lazy(() => import('./views/house/House'));
 
+//chart
+const Statistic = React.lazy(() => import('./views/statistic/Statistic'));
+
 // Base
 const Accordion = React.lazy(() => import('./views/base/accordion/Accordion'));
 const Breadcrumbs = React.lazy(() => import('./views/base/breadcrumbs/Breadcrumbs'));
@@ -71,6 +74,7 @@ const routes = [
     { path: '/', exact: true, name: 'Home' },
     { path: '/dashboard/contract', name: 'Contract', element: Contract },
     { path: '/dashboard/house', name: 'House', element: House },
+    { path: '/dashboard/statistic', name: 'Staitstic', element: Statistic },
     { path: '/dashboard', name: 'Dashboard', element: Dashboard },
     { path: '/theme', name: 'Theme', element: Colors, exact: true },
     { path: '/theme/colors', name: 'Colors', element: Colors },
